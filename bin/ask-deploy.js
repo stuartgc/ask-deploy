66005a48e4e03ffe004d7e9ece5035b4d8e4c17f
Stuart Chandra <schandra2@hearst.com>
2018-02-02 16:46:19 -0800

    working reading and creation of .ask/config

--- bin/ask-deploy.js
+++ bin/ask-deploy.js
@@ -1,36 +1,40 @@
 #!/usr/bin/env node
 "use strict";
 
-if ( !require( "semver" ).gte( process.version, "6.10.0")) {
+if ( !require( "semver" ).gte( process.version, "6.10.2")) {
     console.log( "Version of node.js doesn't meet minimum requirement.") ;
-    console.log( "Please ensure system has node.js version 6.10.0 or higher." );
+    console.log( "Please ensure system has node.js version 6.10.2 or higher." );
 
     return;
 }
 
-const commander = require( "commander" );
+const deploy = require( "./../lib/deploy/deploy" ),
+      program = require( "commander" );
 
-commander
+let configEnv = null,
+    deployTarget = null;
+
+//TODO: add support for debug flag
+program
+.usage( 'ask-deploy <env> [options]' )
 .description( "Deploy wrapper for Command Line Interface for Alexa Skill Management API" )
-.usage( '[options] <config_name ...>' )
-.option( '-t, --target <target>', 'deploy "lambda", "model", "skill" or "all"' )
-.option( '-h, --help', 'output usage information', () => {
-    console.log( CONSTANTS.COMMAND.DEPLOY.HELP_DESCRIPTION );
-
-    process.exit( 0 );
-})
+.option( '-t, --target <target>', 'deploy "lambda", "model", "skill" or "all"', /^(lambda|model|skill|all)$/i, 'all' )
 .option( "-v, --version", "output the version number of ask-deploy" , () => {
     console.log( require( "../package.json" ).version );
 
     process.exit( 0 );
 } )
+.action( function( env, options ) {
+    configEnv = env;
+
+    deployTarget = options.target;// || "all";
+} )
 .parse( process.argv );
 
-if ( !process.argv.slice( 2 ).length ) {
-    commander.outputHelp();
+if ( configEnv && deployTarget ) {
+    deploy.deploy( configEnv, deployTarget );
 } else {
-    if ( [ 'deploy', 'help' ]
-        .indexOf( process.argv[ 2 ]) === -1 ) {
-        console.log( 'Command not recognized. Please run "ask-deploy" for help.' );
-    }
+    program.outputHelp();
+
+    process.exit( 1 );
 }
